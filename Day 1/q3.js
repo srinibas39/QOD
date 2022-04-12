e655857d9be3668ade718f9936f8c7cfa342e136
srinibas <srinibaskhuntia39@gmail.com>
2022-04-12 19:06:39 +0530

    bigs fixed

--- Day 1/q3.js
+++ Day 1/q3.js
@@ -3,7 +3,7 @@
 const shiftingCharacter = (str, n) => {
     let newStr = [];
     for (let i = 0; i < str.length; i++) {
-        newStr.push(String.fromCharCode(str[i].charCodeAt(0)+2));   
+        newStr.push(String.fromCharCode(str[i].charCodeAt(0)+n));   
     }
     return newStr.join("");
 }
