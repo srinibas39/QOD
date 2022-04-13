f5e315233ab84550bfd8365bf38bfe9ba8bcebae
srinibas <srinibaskhuntia39@gmail.com>
2022-04-13 18:43:40 +0530

    numbers fixed

--- Day 2/q1.js
+++ Day 2/q1.js
@@ -1,7 +1,12 @@
 
 
-const areaOfHexagon=(len)=>((((3*Math.sqrt(3)*(len**2))/2)*100)/100).toFixed(2);
-
+const areaOfHexagon=(len)=>{
+    const area=((3*Math.sqrt(3)*(len**2))/2);
+    const areaArr=(area+"").split(".")
+    const decimal=areaArr[1].slice(0,2);
+    const res=areaArr[0]+"."+decimal
+    return res;
+}
 const a=areaOfHexagon(10);
 
 console.log(a);
